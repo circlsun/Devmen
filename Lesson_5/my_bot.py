2d6f4e54d6ddd7f63e523164e653a753cef9412e
circlsun <circlsun@gmail.com>
2022-11-19 17:51:19 +0300

    work

--- Lesson_5/my_bot.py
+++ Lesson_5/my_bot.py
@@ -1,8 +1,19 @@
-import smtplib
+import ptbot
 import os
+from pytimeparse import parse
 from dotenv import load_dotenv
 
 load_dotenv()
-TG_TOKEN = os.environ['TG_TOKEN']
-TG_CHAT_ID = os.environ['TG_CHAT_ID']
-print(TG_TOKEN)
+TG_TOKEN = os.getenv('TELEG_TOKEN')
+TG_CHAT_ID = os.getenv('TELEG_CHAT_ID')
+
+def wait(chat_id, question):
+    timer_s = parse(int(question))
+    bot.create_timer(6, choose, author_id=chat_id, message=question)
+
+def choose(author_id, message):
+    bot.send_message(author_id, "Время вышло!")
+
+bot = ptbot.Bot(TG_TOKEN)
+bot.reply_on_message(wait)
+bot.run_bot()
