0c7ac0d0fd3d8d43864d2a0d4dcc12d8ec97a315
Matveev Andrey <116314099+circlsun@users.noreply.github.com>
2022-11-22 13:31:15 +0300

    Update my_bot.py

--- Lesson_5/my_bot.py
+++ Lesson_5/my_bot.py
@@ -7,13 +7,33 @@ load_dotenv()
 TG_TOKEN = os.getenv('TELEG_TOKEN')
 TG_CHAT_ID = os.getenv('TELEG_CHAT_ID')
 
-def wait(chat_id, question):
-    timer_s = parse(int(question))
-    bot.create_timer(6, choose, author_id=chat_id, message=question)
+def main():
 
-def choose(author_id, message):
-    bot.send_message(author_id, "Время вышло!")
+    def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
+        iteration = min(total, iteration)
+        percent = "{0:.1f}"
+        percent = percent.format(100 * (iteration / float(total)))
+        filled_length = int(length * iteration // total)
+        pbar = fill * filled_length + zfill * (length - filled_length)
+        return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
-bot = ptbot.Bot(TG_TOKEN)
-bot.reply_on_message(wait)
-bot.run_bot()
+    def reply(chat_id, text):
+        timer_s = parse(text)
+        message_id = bot.send_message(chat_id, "Запускаю таймер!")
+        bot.create_countdown(timer_s, notify, msg_id=message_id, author_id=chat_id, timer=timer_s)
+        bot.create_timer(timer_s, set_end, author_id=chat_id)
+       
+    def set_end(author_id):
+        bot.send_message(author_id, 'Время вышло!')
+    
+    def notify(secs_left, author_id, msg_id, timer):
+      message = f"Осталось {secs_left} секунд!\n{render_progressbar(timer, secs_left)}"
+      bot.update_message(author_id, msg_id,  message)
+          
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(reply)
+    bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
